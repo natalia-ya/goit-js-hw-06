15ca77a1b76af81830e9076b7533f5b835a460dd
natalia-ya <sagaidak.nata@gmail.com>
2021-11-16 22:44:08 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,11 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const addItems = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const newEl = document.createElement('li');
+  newEl.classList.add('item');
+  newEl.textContent = ingredient;
+  return newEl;
+});
+addItems.append(...elements);
