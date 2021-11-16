9b76d4ee8dbb7b23a962c767fe0e271081c69907
natalia-ya <sagaidak.nata@gmail.com>
2021-11-16 22:48:54 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const randomColor = document.querySelector('.widget');
+const span = document.querySelector('.color');
+randomColor.addEventListener('click', onButton);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButton(event) {
+  span.textContent = getRandomHexColor();
+  if (!event.target.classList.contains('change-color')) {
+    return;
+  }
+  return (document.body.style.background = getRandomHexColor());
 }
